d43df4bf68d199532d7ad30342281d18722c05be
Mohammed Omran <119889766+Omran950@users.noreply.github.com>
2024-10-18 14:13:38 +0300

    Update reporter.js

--- spec/helpers/reporter.js
+++ spec/helpers/reporter.js
@@ -1,12 +1,10 @@
 const SpecReporter = require("jasmine-spec-reporter").SpecReporter;
 
-jasmine.getEnv().clearReporters(); // remove default reporter logs
+jasmine.getEnv().clearReporters(); 
 jasmine.getEnv().addReporter(
   new SpecReporter({
-    // add jasmine-spec-reporter
     spec: {
       displayPending: true,
     },
   })
 );
-s
